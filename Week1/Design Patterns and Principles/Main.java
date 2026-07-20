9bd9b5b97b06103556e36797657b0c5802a4d77b
Rushikesh <jilhawarrushikesh@gmail.com>
2026-07-21 01:50:40 +0530

    Implementing the Builder Pattern Scenario

---
+++ Week1/Design Patterns and Principles/Main.java
@@ -0,0 +1,84 @@
+class Computer {
+
+    private String cpu;
+    private String ram;
+    private String storage;
+    private String graphicsCard;
+    private String operatingSystem;
+
+    private Computer(Builder builder) {
+        this.cpu = builder.cpu;
+        this.ram = builder.ram;
+        this.storage = builder.storage;
+        this.graphicsCard = builder.graphicsCard;
+        this.operatingSystem = builder.operatingSystem;
+    }
+
+    public void showConfiguration() {
+        System.out.println("Computer Configuration:");
+        System.out.println("CPU : " + cpu);
+        System.out.println("RAM : " + ram);
+        System.out.println("Storage : " + storage);
+        System.out.println("Graphics Card : " + graphicsCard);
+        System.out.println("Operating System : " + operatingSystem);
+        System.out.println();
+    }
+    public static class Builder {
+
+        private String cpu;
+        private String ram;
+        private String storage;
+        private String graphicsCard;
+        private String operatingSystem;
+
+        public Builder setCPU(String cpu) {
+            this.cpu = cpu;
+            return this;
+        }
+
+        public Builder setRAM(String ram) {
+            this.ram = ram;
+            return this;
+        }
+
+        public Builder setStorage(String storage) {
+            this.storage = storage;
+            return this;
+        }
+
+        public Builder setGraphicsCard(String graphicsCard) {
+            this.graphicsCard = graphicsCard;
+            return this;
+        }
+
+        public Builder setOperatingSystem(String operatingSystem) {
+            this.operatingSystem = operatingSystem;
+            return this;
+        }
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Computer gamingPC = new Computer.Builder()
+                .setCPU("Intel i9")
+                .setRAM("32 GB")
+                .setStorage("1 TB SSD")
+                .setGraphicsCard("NVIDIA RTX 4080")
+                .setOperatingSystem("Windows 11")
+                .build();
+        Computer officePC = new Computer.Builder()
+                .setCPU("Intel i5")
+                .setRAM("16 GB")
+                .setStorage("512 GB SSD")
+                .setOperatingSystem("Windows 10")
+                .build();
+        gamingPC.showConfiguration();
+        officePC.showConfiguration();
+    }
+}
