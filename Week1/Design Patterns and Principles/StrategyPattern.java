510e1f51f819fc39a59e17b3d7183509001c7f4e
Rushikesh <jilhawarrushikesh@gmail.com>
2026-07-21 11:12:59 +0530

    Implementing the Strategy Pattern Scenario

---
+++ Week1/Design Patterns and Principles/StrategyPattern.java
@@ -0,0 +1,39 @@
+interface PaymentStrategy {
+    void pay(double amount);
+}
+
+class CreditCardPayment implements PaymentStrategy {
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Credit Card");
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using PayPal");
+    }
+}
+
+class PaymentContext {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void executePayment(double amount) {
+        paymentStrategy.pay(amount);
+    }
+}
+
+public class StrategyPattern {
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext();
+
+        context.setPaymentStrategy(new CreditCardPayment());
+        context.executePayment(5000);
+
+        context.setPaymentStrategy(new PayPalPayment());
+        context.executePayment(2500);
+    }
+}
